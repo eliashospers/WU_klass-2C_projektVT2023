5d9a9f184f22e006c6264124c9cd44d897783f79
eliashospers <elias.hospers@elev.ga.ntig.se>
2023-04-02 23:06:06 +0200

    sista inlämning
    lade inspelningarna i misc mappen

--- js/script.js
+++ js/script.js
@@ -17,12 +17,12 @@ burger.addEventListener('click', toggleMenu)
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
